6e04b85b5356e8e387fe2ed7b1f77bb8ca7b75a4
Jordan <par16042@byui.edu>
2018-09-30 18:28:50 -0600

    Prove 02 - KNN

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import csv
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+
 from HardCodedClassifier import HardCodedClassifier
 
+# K is the number of nearest neighbors
+k = 1
 iris = datasets.load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=29)
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=20)
 
 
 def main():
     # Show the data (the attributes of each instance)
-    print(iris.data)
+    # print(iris.data)
 
     # Show the target values (in numeric format) of each instance
-    print(iris.target)
+    # print(iris.target)
 
     # Show the actual target names that correspond to each number
-    print(iris.target_names)
+    # print(iris.target_names)
 
     # print "\nX Train: \n", X_train
     # print "\nX Test: \n", X_test
@@ -25,43 +28,48 @@ def main():
 
     # Naive Bayes algorithm implementation GaussianNB to train a model
     # classifier = GaussianNB()
-    classifier = HardCodedClassifier()
+
+    # classifier = HardCodedClassifier()
+    # model = classifier.fit(X_train, y_train)
+
+    classifier = HardCodedClassifier(k)
     model = classifier.fit(X_train, y_train)
 
     targets_predicted = model.predict(X_test)
 
-    itemsCorrect = 0
-    notCorrect = 0
+    items_correct = 0
+    not_correct = 0
 
     # Compare the predictions with the answers
     for i in range(len(targets_predicted)):
         if targets_predicted[i] == y_test[i]:
-            itemsCorrect += 1
+            items_correct += 1
         else:
-            notCorrect += 1
+            not_correct += 1
 
-    totalItems = itemsCorrect + notCorrect
+    total_items = items_correct + not_correct
 
     # Calculating and displaying the amount correct
-    print "\n\nMachine got ", itemsCorrect, " correct out of ", totalItems, ". Accuracy is at ", \
-        100.00 * itemsCorrect / totalItems, "%.\n"
+    print "\n\nMachine got ", items_correct, " correct out of ", total_items, ". Accuracy is at ", \
+        100.00 * items_correct / total_items, "%.\n"
 
-    # Using a text file and retrieving data from it
-    with open('C:\Users\Jordan\Desktop\PycharmProjects\prove01\employee_brithday.txt') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
 
-        # Loop
-        for row in csv_reader:
-            if line_count == 0:
-                print "Column names are", row
-                line_count += 1
-            else:
-                print "\t", row[0], "works in the", row[1], "department, and was born in", row[2]
-                line_count += 1
-
-        # Tells us how many lines were used
-        print "Processed", line_count, "lines."
+    # # Using a text file and retrieving data from it
+    # with open('C:\Users\Jordan\Desktop\PycharmProjects\prove01\employee_brithday.txt') as csv_file:
+    #     csv_reader = csv.reader(csv_file, delimiter=',')
+    #     line_count = 0
+    #
+    #     # Loop
+    #     for row in csv_reader:
+    #         if line_count == 0:
+    #             print "Column names are", row
+    #             line_count += 1
+    #         else:
+    #             print "\t", row[0], "works in the", row[1], "department, and was born in", row[2]
+    #             line_count += 1
+    #
+    #     # Tells us how many lines were used
+    #     print "Processed", line_count, "lines."
 
 
 if __name__ == "__main__":
